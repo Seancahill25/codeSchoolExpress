780cbe715db11b0ce36a1632bfb1471c42dce121
Sean Cahill <seancahill25@gmail.com>
2018-01-28 04:51:26 +0000

    .

--- level_4/app.js
+++ level_4/app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+
 var cities = {
    "Boston": "Massachusetts",
    "Providence": "Rhode Island", 
@@ -9,13 +10,16 @@ var cities = {
    "Warwick": "Rhode Island",
    "New York City": "New York"
 };
+
 app.use(express.static('public'));
+
 app.param('city', function(req, res, next) {
    var name = req.params.city;
    var city = name[0].toUpperCase() + name.slice(1).toLowerCase();
    req.cityName = city;
    next();
 });
+
 app.get('/cities', function(req, res){
     if (req.query.limit >= 0) { 
         res.json(cities.slice(0, req.query.limit));
@@ -25,6 +29,7 @@ app.get('/cities', function(req, res){
       res.json(Object.keys(cities));
    }
 });
+
 app.get("/cities/:city", function(req, res) {
    var description = cities[req.cityName];
    if (!description) {
@@ -33,6 +38,7 @@ app.get("/cities/:city", function(req, res) {
       res.json(description);
    }
 });
+
 app.post('/cities', parseUrlencoded, function(req, res) {
    if (req.body.city.length > 4 || req.body.state.length > 2) {
       var newCity = req.body;
@@ -42,8 +48,10 @@ app.post('/cities', parseUrlencoded, function(req, res) {
       res.status(400,"Not enough characters");
    }
 });
+
 app.delete('/cities/:city', function(req, res) {
    delete cities[req.cityName];
    res.status(200);
 });
+
 app.listen(process.env.PORT);
